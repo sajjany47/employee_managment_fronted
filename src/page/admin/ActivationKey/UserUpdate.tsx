4f526085b899d8e5073d16870fdf635411ecf71c
sajjany47 <sajjany47@gmail.com>
2023-12-06 23:20:41 +0530

    user update

--- src/page/admin/ActivationKey/UserUpdate.tsx
+++ src/page/admin/ActivationKey/UserUpdate.tsx
@@ -1,6 +1,6 @@
 import { Field, Form, Formik } from "formik";
 import { useLocation } from "react-router-dom";
-import { Grid } from "@mui/material";
+import { Divider, Grid } from "@mui/material";
 import { inputField, selectField } from "../../../components/FieldType";
 import InputAdornment from "@mui/material/InputAdornment";
 import { useSelector } from "react-redux";
@@ -25,17 +25,25 @@ const UserUpdate = () => {
               spacing={{ xs: 2, md: 2 }}
               columns={{ xs: 4, sm: 8, md: 12 }}
             >
-              <Grid item xs={2} sm={4} md={4}>
+              <Grid item xs={12} sm={4} md={3}>
+                <Field
+                  name="activationCode"
+                  label="Activation Code"
+                  component={inputField}
+                  disabled
+                />
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
                 <Field name="name" label="Name" component={inputField} />
               </Grid>
-              <Grid item xs={2} sm={4} md={4}>
+              <Grid item xs={12} sm={4} md={3}>
                 <Field
                   name="username"
                   label="Username"
                   component={inputField}
                 />
               </Grid>
-              <Grid item xs={2} sm={4} md={4}>
+              <Grid item xs={12} sm={4} md={3}>
                 <Field
                   name="email"
                   label="Email"
@@ -43,10 +51,10 @@ const UserUpdate = () => {
                   component={inputField}
                 />
               </Grid>
-              <Grid item xs={2} sm={4} md={4}>
+              <Grid item xs={12} sm={4} md={3}>
                 <Field name="mobile" label="Mobile" component={inputField} />
               </Grid>
-              <Grid item xs={2} sm={4} md={4}>
+              <Grid item xs={12} sm={4} md={3}>
                 <Field
                   type="date"
                   name="dob"
@@ -59,7 +67,7 @@ const UserUpdate = () => {
                   }}
                 />
               </Grid>
-              <Grid item xs={2} sm={4} md={4}>
+              <Grid item xs={12} sm={4} md={3}>
                 <Field
                   name="role"
                   label="Role"
@@ -69,7 +77,35 @@ const UserUpdate = () => {
                   }
                 />
               </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+                <Field
+                  name="password"
+                  label="Password"
+                  component={inputField}
+                  type="password"
+                />
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+                <Field name="address" label="Address" component={inputField} />
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+                <Field name="state" label="State" component={inputField} />
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+                <Field
+                  name="district"
+                  label="District"
+                  component={inputField}
+                />
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+                <Field name="city" label="City" component={inputField} />
+              </Grid>
+              <Grid item xs={12} sm={4} md={3}>
+                <Field name="pincode" label="Pincode" component={inputField} />
+              </Grid>
             </Grid>
+            <Divider>Education Details</Divider>
           </Form>
         )}
       </Formik>
