8bff8e0146e2d5e0628308cd3e827725a2faf940
QbAnurag <anurag.patra@qbent.in>
2023-12-07 18:23:13 +0530

    userupdate

--- src/page/admin/ActivationKey/UserUpdate.tsx
+++ src/page/admin/ActivationKey/UserUpdate.tsx
@@ -1,24 +1,27 @@
-import { Field, Form, Formik } from "formik";
+import { Field, FieldArray, Form, Formik } from "formik";
 import { useLocation } from "react-router-dom";
 import { Divider, Grid } from "@mui/material";
 import { inputField, selectField } from "../../../components/FieldType";
 import InputAdornment from "@mui/material/InputAdornment";
 import { useSelector } from "react-redux";
 import { data } from "../../../shared/Config";
+import { Fragment } from "react";
 
 const UserUpdate = () => {
   const { state } = useLocation();
   console.log(state.data);
   const userType = useSelector((state: any) => state.auth.auth.user);
 
-  const intialValue = {};
+  const intialValue = {
+    education: [{ boardName: "", passingYear: "", marksPercentage: "" }],
+  };
   const userUpdate = (value: any) => {
     console.log(value);
   };
   return (
     <div>
       <Formik initialValues={intialValue} onSubmit={userUpdate}>
-        {({ handleSubmit }) => (
+        {({ handleSubmit, values }) => (
           <Form onSubmit={handleSubmit}>
             <Grid
               container
@@ -106,6 +109,45 @@ const UserUpdate = () => {
               </Grid>
             </Grid>
             <Divider>Education Details</Divider>
+
+            <FieldArray
+              name="education"
+              render={(arrayHelpers) => (
+                <Grid
+                  container
+                  spacing={{ xs: 2, md: 2 }}
+                  columns={{ xs: 4, sm: 8, md: 12 }}
+                >
+                  {values.education && values.education.length > 0
+                    ? values.education.map((item, index) => (
+                        <Fragment key={index}>
+                          <Grid item xs={12} sm={4} md={3}>
+                            <Field
+                              name={`education.${index}.boardName`}
+                              label="Board Name"
+                              component={inputField}
+                            />
+                          </Grid>
+                          <Grid item xs={12} sm={4} md={3}>
+                            <Field
+                              name={`education.${index}.passingYear`}
+                              label="Passing Year"
+                              component={inputField}
+                            />
+                          </Grid>
+                          <Grid item xs={12} sm={4} md={3}>
+                            <Field
+                              name={`education.${index}.marksPercentage`}
+                              label="Marks (%)"
+                              component={inputField}
+                            />
+                          </Grid>
+                        </Fragment>
+                      ))
+                    : null}
+                </Grid>
+              )}
+            />
           </Form>
         )}
       </Formik>
