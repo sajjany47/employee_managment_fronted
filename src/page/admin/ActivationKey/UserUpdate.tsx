7044d2f2c7d0de52cabc3d6f4a7ec3a048124b09
QbAnurag <anurag.patra@qbent.in>
2023-12-08 17:39:17 +0530

    working details add

--- src/page/admin/ActivationKey/UserUpdate.tsx
+++ src/page/admin/ActivationKey/UserUpdate.tsx
@@ -17,6 +17,14 @@ const UserUpdate = () => {
 
   const intialValue = {
     education: [{ boardName: "", passingYear: "", marksPercentage: "" }],
+    workDetail: [
+      {
+        companyName: "",
+        position: "",
+        startingYear: "",
+        endingYear: "",
+      },
+    ],
   };
   const userUpdate = (value: any) => {
     console.log(value);
@@ -179,6 +187,92 @@ const UserUpdate = () => {
                           boardName: "",
                           passingYear: "",
                           marksPercentage: "",
+                        })
+                      }
+                    >
+                      Add More
+                    </Button>
+                  </Grid>
+                </Box>
+              )}
+            />
+
+            <Divider sx={{ margin: "20px" }}>Working Details</Divider>
+
+            <FieldArray
+              name="workDetail"
+              render={(arrayHelpers) => (
+                <Box sx={{ marginTop: "10px" }}>
+                  {values.workDetail &&
+                    values.workDetail.length > 0 &&
+                    values.workDetail.map((item, index) => (
+                      <Grid
+                        sm={12}
+                        xs={12}
+                        key={index}
+                        container
+                        spacing={{ xs: 2, md: 2 }}
+                        columns={{ xs: 4, sm: 8, md: 12 }}
+                        sx={{ marginBottom: "15px" }}
+                      >
+                        <Grid item xs={12} sm={4} md={3}>
+                          <Field
+                            name={`workDetail.${index}.companyName`}
+                            label="Company Name"
+                            component={inputField}
+                          />
+                        </Grid>
+                        <Grid item xs={12} sm={4} md={3}>
+                          <Field
+                            name={`workDetail.${index}.position`}
+                            label="Position"
+                            component={inputField}
+                          />
+                        </Grid>
+                        <Grid item xs={12} sm={4} md={3}>
+                          <Field
+                            name={`workDetail.${index}.startingYear`}
+                            label="Starting Year"
+                            component={inputField}
+                          />
+                        </Grid>
+                        <Grid item xs={12} sm={4} md={3}>
+                          <Field
+                            name={`workDetail.${index}.endingYear`}
+                            label="Ending Year"
+                            component={inputField}
+                          />
+                        </Grid>
+
+                        <IconButton
+                          aria-label="delete"
+                          onClick={() => arrayHelpers.remove(index)}
+                          sx={{
+                            ":hover": {
+                              backgroundColor: "whitesmoke",
+                            },
+                          }}
+                        >
+                          <DeleteIcon
+                            sx={{
+                              fontSize: "30px",
+                              margin: "auto",
+                              color: "red",
+                            }}
+                          />
+                        </IconButton>
+                      </Grid>
+                    ))}
+                  <Grid xs={12} sm={12}>
+                    <Button
+                      variant="outlined"
+                      startIcon={<AddIcon />}
+                      onClick={() =>
+                        arrayHelpers.push({
+                          companyName: "",
+                          position: "",
+                          startingYear: "",
+                          endingYear: "",
                         })
                       }
                     >
