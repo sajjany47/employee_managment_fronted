cebd68556bacfda3842d48a512c531844af5bc06
sajjany47 <sajjany47@gmail.com>
2023-12-10 21:03:58 +0530

    mui date picker uninstall

--- src/components/FieldType.tsx
+++ src/components/FieldType.tsx
@@ -4,11 +4,6 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 
-import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-
-import { MobileDatePicker } from "@mui/x-date-pickers";
-
 interface MyFormValues {
   field: any;
   form: { touched: any; errors: any };
@@ -30,27 +25,6 @@ export const inputField = ({
   return (
     <>
       <TextField {...field} {...props} variant="outlined" className="w-full" />
-      {errors[field.name] && touched[field.name] && (
-        <small style={{ color: "red" }}>{errors[field.name]}</small>
-      )}
-    </>
-  );
-};
-
-export const dateField = ({
-  field,
-  form: { touched, errors },
-  ...props
-}: MyFormValues) => {
-  return (
-    <>
-      <LocalizationProvider dateAdapter={AdapterMoment}>
-        <MobileDatePicker
-          {...field}
-          {...props}
-          renderInput={(params: any) => <TextField {...params} fullWidth />}
-        />
-      </LocalizationProvider>
       {errors[field.name] && touched[field.name] && (
         <small style={{ color: "red" }}>{errors[field.name]}</small>
       )}
