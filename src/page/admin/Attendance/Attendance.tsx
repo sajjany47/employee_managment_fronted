89dfe89c218f7fb9df33e5f7bee5041efe9cc20c
QbAnurag <anurag.patra@qbent.in>
2023-12-28 19:18:46 +0530

    changes

--- src/page/admin/Attendance/Attendance.tsx
+++ src/page/admin/Attendance/Attendance.tsx
@@ -1,5 +1,41 @@
+import { Box, Button, Grid, TextField } from "@mui/material";
+
 const Attendance = () => {
-  return <div>Attendance</div>;
+  return (
+    <>
+      <Grid container rowSpacing={2} columnSpacing={2}>
+        <Grid item xs={12}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={8} md={8}>
+              <h6>
+                <strong>Attendance Details</strong>
+              </h6>
+            </Grid>
+            <Grid item xs={12} sm={8} md={4}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={6}>
+                  <TextField
+                    label="Search"
+                    id="outlined-size-small"
+                    size="small"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6} md={6}>
+                  <Button
+                    variant="contained"
+                    // startIcon={<AddIcon />}
+                    // onClick={handleClickOpen}
+                  >
+                    Attendance
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Attendance;
