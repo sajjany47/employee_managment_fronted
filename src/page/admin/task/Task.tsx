6b71709870fdc2e158c707e0e718f130568120cc
sajjany47 <sajjany47@gmail.com>
2024-03-24 16:31:24 +0530

    chnages

--- src/page/admin/task/Task.tsx
+++ src/page/admin/task/Task.tsx
@@ -6,6 +6,7 @@ import {
   Dialog,
   DialogContent,
   DialogTitle,
+  Divider,
   FormControl,
   Grid,
   MenuItem,
@@ -128,148 +129,75 @@ const Task = () => {
               </Tabs>
             </Box>
             <CustomTabPanel value={value} index={0}>
-              <ul className="grid grid-cols-1 xl:grid-cols-3 gap-y-10 gap-x-6 items-start p-8">
-                <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
-                  <div className="order-1 sm:ml-6 xl:ml-0">
-                    <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
-                      <span className="mb-1 block text-sm leading-6 text-indigo-500">
-                        Headless UI
-                      </span>
-                      Completely unstyled, fully accessible UI components
-                    </h3>
-                    <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
-                      <p>
-                        Completely unstyled, fully accessible UI components,
-                        designed to integrate beautifully with Tailwind CSS.
-                      </p>
+              <div className="w-full">
+                <div className="p-2 pb-10">
+                  <h1 className="text-2xl font-semibold text-gray-800 mt-4">
+                    The Magnificent Bogra
+                  </h1>
+                  <p className="text-xl text-gray-400 mt-2 leading-relaxed">
+                    Located in Rajshahi Division, Bogra is one of the oldest and
+                    most fascinating towns in Bangladesh
+                  </p>
+                </div>
+                <div className="bg-blue-50 p-5">
+                  <div className="sm:flex sm:justify-between">
+                    <div>
+                      <div className="text-lg text-gray-700">
+                        <span className="text-gray-900 font-bold">196 km</span>{" "}
+                        from Dhaka
+                      </div>
+                      <div className="flex items-center">
+                        <div className="flex">
+                          <svg
+                            className="w-4 h-4 mx-px fill-current text-green-600"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 14 14"
+                          >
+                            <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
+                          </svg>
+                          <svg
+                            className="w-4 h-4 mx-px fill-current text-green-600"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 14 14"
+                          >
+                            <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
+                          </svg>
+                          <svg
+                            className="w-4 h-4 mx-px fill-current text-green-600"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 14 14"
+                          >
+                            <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
+                          </svg>
+                          <svg
+                            className="w-4 h-4 mx-px fill-current text-green-600"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 14 14"
+                          >
+                            <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
+                          </svg>
+                          <svg
+                            className="w-4 h-4 mx-px fill-current text-green-600"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 14 14"
+                          >
+                            <path d="M6.43 12l-2.36 1.64a1 1 0 0 1-1.53-1.11l.83-2.75a1 1 0 0 0-.35-1.09L.73 6.96a1 1 0 0 1 .59-1.8l2.87-.06a1 1 0 0 0 .92-.67l.95-2.71a1 1 0 0 1 1.88 0l.95 2.71c.13.4.5.66.92.67l2.87.06a1 1 0 0 1 .59 1.8l-2.3 1.73a1 1 0 0 0-.34 1.09l.83 2.75a1 1 0 0 1-1.53 1.1L7.57 12a1 1 0 0 0-1.14 0z"></path>
+                          </svg>
+                        </div>
+                        <div className="text-gray-600 ml-2 text-sm md:text-base mt-1">
+                          16 reviews
+                        </div>
+                      </div>
                     </div>
-                    <a
-                      className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
-                      href="https://headlessui.dev"
-                    >
-                      Learn more
-                      <span className="sr-only">
-                        , Completely unstyled, fully accessible UI components
-                      </span>
-                      <svg
-                        className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
-                        width={3}
-                        height={6}
-                        viewBox="0 0 3 6"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="M0 0L3 3L0 6" />
-                      </svg>
-                    </a>
+                    <button className="mt-3 sm:mt-0 py-2 px-5 md:py-3 md:px-6 bg-purple-700 hover:bg-purple-600 font-bold text-white md:text-lg rounded-lg shadow-md">
+                      Book Ticket
+                    </button>
                   </div>
-                  <img
-                    src="https://tailwindcss.com/_next/static/media/headlessui@75.c1d50bc1.jpg"
-                    alt=""
-                    className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full"
-                    width={1216}
-                    height={640}
-                  />
-                </li>
-                <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
-                  <div className="order-1 sm:ml-6 xl:ml-0">
-                    <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
-                      <span className="mb-1 block text-sm leading-6 text-purple-500">
-                        Heroicons
-                      </span>
-                      Beautiful hand-crafted SVG icons, by the makers of
-                      Tailwind CSS.
-                    </h3>
-                    <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
-                      <p>
-                        A set of 450+ free MIT-licensed SVG icons. Available as
-                        basic SVG icons and via first-party React and Vue
-                        libraries.
-                      </p>
-                    </div>
-                    <a
-                      className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
-                      href="https://heroicons.com"
-                    >
-                      Learn more
-                      <span className="sr-only">
-                        , Beautiful hand-crafted SVG icons, by the makers of
-                        Tailwind CSS.
-                      </span>
-                      <svg
-                        className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
-                        width={3}
-                        height={6}
-                        viewBox="0 0 3 6"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="M0 0L3 3L0 6" />
-                      </svg>
-                    </a>
+                  <div className="mt-3 text-gray-600 text-sm md:text-sm">
+                    *Places to visit: Mahasthangarh, Vasu Bihar &amp; Momo Inn
                   </div>
-                  <img
-                    src="https://tailwindcss.com/_next/static/media/heroicons@75.4a558f35.jpg"
-                    alt=""
-                    className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full"
-                    width={1216}
-                    height={640}
-                  />
-                </li>
-                <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
-                  <div className="order-1 sm:ml-6 xl:ml-0">
-                    <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
-                      <span className="mb-1 block text-sm leading-6 text-cyan-500">
-                        Hero Patterns
-                      </span>
-                      Seamless SVG background patterns by the makers of Tailwind
-                      CSS.
-                    </h3>
-                    <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
-                      <p>
-                        A collection of over 100 free MIT-licensed high-quality
-                        SVG patterns for you to use in your web projects.
-                      </p>
-                    </div>
-                    <a
-                      className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
-                      href="https://heropatterns.com"
-                    >
-                      Learn more
-                      <span className="sr-only">
-                        , Seamless SVG background patterns by the makers of
-                        Tailwind CSS.
-                      </span>
-                      <svg
-                        className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
-                        width={3}
-                        height={6}
-                        viewBox="0 0 3 6"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="M0 0L3 3L0 6" />
-                      </svg>
-                    </a>
-                  </div>
-                  <img
-                    src="https://tailwindcss.com/_next/static/media/heropatterns@75.82a09697.jpg"
-                    alt=""
-                    className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full"
-                    width={1216}
-                    height={640}
-                  />
-                </li>
-              </ul>
+                </div>
+              </div>
             </CustomTabPanel>
             <CustomTabPanel value={value} index={1}>
               Item Two
