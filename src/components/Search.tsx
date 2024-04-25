81692f240f34748c4ebba58efef35ffa71af857e
QbAnurag <anurag.patra@qbent.in>
2024-04-25 19:11:34 +0530

    changes

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { Form, Formik } from "formik";
 import { InputField, SelectField } from "./DynamicField";
 import { ConfigData } from "../shared/ConfigData";
@@ -6,7 +6,8 @@ import { EmployeeServices } from "../page/admin/EmployeeList/EmployeeServices";
 import { enqueueSnackbar } from "notistack";
 import { useEffect, useState } from "react";
 
-const Search = () => {
+const Search = (props: any) => {
+  const data = props?.value;
   const employeeServices = new EmployeeServices();
   const [countryData, setCountryData] = useState([]);
   const [stateData, setStateData] = useState([]);
@@ -51,7 +52,7 @@ const Search = () => {
 
   return (
     <div>
-      <Formik onSubmit={handelSearch} initialValues={{}}>
+      <Formik onSubmit={handelSearch} initialValues={{ ...data }}>
         {({ handleSubmit, setFieldValue }) => (
           <Form onSubmit={handleSubmit}>
             <Grid
@@ -59,66 +60,127 @@ const Search = () => {
               spacing={{ xs: 2, md: 2 }}
               columns={{ xs: 4, sm: 8, md: 12 }}
             >
-              <Grid item xs={6} sm={3} md={3}>
-                <InputField name="activationCode" label="Activation Code" />
-              </Grid>
+              {data?.activationCode && (
+                <Grid item xs={6} sm={3} md={3}>
+                  <InputField name="activationCode" label="Activation Code" />
+                </Grid>
+              )}
+              {data?.name && (
+                <Grid item xs={6} sm={3} md={3}>
+                  <InputField name="name" label="Name" />
+                </Grid>
+              )}
 
-              <Grid item xs={6} sm={3} md={3}>
-                <InputField name="name" label="Name" />
-              </Grid>
-              <Grid item xs={6} sm={3} md={3}>
-                <InputField name="username" label="Username" />
-              </Grid>
-              <Grid item xs={6} sm={3} md={3}>
-                <InputField name="email" label="Email" type="email" />
-              </Grid>
-              <Grid item xs={6} sm={3} md={3}>
-                <InputField name="mobile" label="Mobile" />
-              </Grid>
-              <Grid item xs={6} sm={3} md={3}>
-                <InputField name="position" label="Position" />
-              </Grid>
-              <Grid item xs={6} sm={4} md={3}>
-                <SelectField
-                  name="role"
-                  label="Role"
-                  options={ConfigData.adminType}
-                />
-              </Grid>
-              <Grid item xs={6} sm={4} md={3}>
-                <SelectField
-                  name="country"
-                  label="Country"
-                  options={countryData}
-                  onChange={(e: any) => handelCountry(setFieldValue, e)}
-                />
-              </Grid>
-              <Grid item xs={6} sm={4} md={3}>
-                <SelectField
-                  name="state"
-                  label="State"
-                  options={stateData}
-                  onChange={(e: any) => setFieldValue("state", e.target.value)}
-                />
-              </Grid>
+              {data?.username && (
+                <Grid item xs={6} sm={3} md={3}>
+                  <InputField name="username" label="Username" />
+                </Grid>
+              )}
 
-              <Grid item xs={6} sm={4} md={3}>
-                <InputField name="pincode" label="Pincode" />
-              </Grid>
-              <Grid item xs={6} sm={4} md={3}>
-                <SelectField
-                  name="activeStatus"
-                  label="Active"
-                  options={ConfigData.activeStatus}
-                />
-              </Grid>
-              <Grid item xs={6} sm={4} md={3}>
-                <SelectField
-                  name="registrationStatus"
-                  label="Status"
-                  options={ConfigData.registrationStatus}
-                />
-              </Grid>
+              {data?.email && (
+                <Grid item xs={6} sm={3} md={3}>
+                  <InputField name="email" label="Email" type="email" />
+                </Grid>
+              )}
+
+              {data?.mobile && (
+                <Grid item xs={6} sm={3} md={3}>
+                  <InputField name="mobile" label="Mobile" />
+                </Grid>
+              )}
+
+              {data?.position && (
+                <Grid item xs={6} sm={3} md={3}>
+                  <InputField name="position" label="Position" />
+                </Grid>
+              )}
+
+              {data?.role && (
+                <Grid item xs={6} sm={4} md={3}>
+                  <SelectField
+                    name="role"
+                    label="Role"
+                    options={ConfigData.adminType}
+                  />
+                </Grid>
+              )}
+
+              {data?.country && (
+                <Grid item xs={6} sm={4} md={3}>
+                  <SelectField
+                    name="country"
+                    label="Country"
+                    options={countryData}
+                    onChange={(e: any) => handelCountry(setFieldValue, e)}
+                  />
+                </Grid>
+              )}
+
+              {data?.state && (
+                <Grid item xs={6} sm={4} md={3}>
+                  <SelectField
+                    name="state"
+                    label="State"
+                    options={stateData}
+                    onChange={(e: any) =>
+                      setFieldValue("state", e.target.value)
+                    }
+                  />
+                </Grid>
+              )}
+
+              {data?.pincode && (
+                <Grid item xs={6} sm={4} md={3}>
+                  <InputField name="pincode" label="Pincode" />
+                </Grid>
+              )}
+
+              {data?.activeStatus && (
+                <Grid item xs={6} sm={4} md={3}>
+                  <SelectField
+                    name="activeStatus"
+                    label="Active"
+                    options={ConfigData.activeStatus}
+                  />
+                </Grid>
+              )}
+
+              {data?.registrationStatus && (
+                <Grid item xs={6} sm={4} md={3}>
+                  <SelectField
+                    name="registrationStatus"
+                    label="Status"
+                    options={ConfigData.registrationStatus}
+                  />
+                </Grid>
+              )}
+              {data && Object.keys(data).length > 0 && (
+                <Grid
+                  item
+                  xs={12}
+                  sm={12}
+                  md={12}
+                  sx={{
+                    display: "flex",
+                    justifyContent: "end",
+                    gap: "5px",
+                  }}
+                >
+                  <Button
+                    //   onClick={handleClose}
+                    variant="contained"
+                    sx={{
+                      backgroundColor: "red",
+                      ":hover": { backgroundColor: "red" },
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                  <Button variant="contained" type="submit">
+                    Submit
+                  </Button>
+                </Grid>
+              )}
             </Grid>
           </Form>
         )}
