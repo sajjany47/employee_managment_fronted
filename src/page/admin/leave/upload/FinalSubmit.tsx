69f24f74b231ab35b2a7f29cd40c3218dc3ea937
QbAnurag <anurag.patra@qbent.in>
2024-05-20 11:31:37 +0530

    changes

--- src/page/admin/leave/upload/FinalSubmit.tsx
+++ src/page/admin/leave/upload/FinalSubmit.tsx
@@ -61,11 +61,10 @@ const FinalSubmit = (props: any) => {
               })}
             </ul>
           </div>
-        ) : (
-          <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 rounded-lg">
-            <p className="text-lg font-semibold">Something went wrong</p>
-          </div>
-        )}
+        ) : // <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 rounded-lg">
+        //   <p className="text-lg font-semibold">Something went wrong</p>
+        // </div>
+        null}
       </div>
     </>
   );
